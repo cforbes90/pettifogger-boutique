deff651314dabe8b5a4fe66570e98e333755e683
shrfu31 <55064372+shrfu31@users.noreply.github.com>
2019-10-23 23:57:02 -0400

    Fully finished Customer level requirements. Going to add a manager level

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -30,9 +30,7 @@ connection.connect(function (err) {
     //buyBuyBuy();
     // connection.end();
 });
-
 function checkInventory() {
-
     console.log("Checking current inventory...", "\n");
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
@@ -41,25 +39,18 @@ function checkInventory() {
         SQLTableData = res;
         console.log(SQLTableData);
         // console.log('SQLTableData 0:', SQLTableData[0]);
-        console.log("Getting id of  first one")
-        console.log(SQLTableData[0].id);
+        // console.log("Getting id of  first one")
+        // console.log(SQLTableData[0].id);
 
-        console.log("Above should be id of first one")
+        // console.log("Above should be id of first one")
         if (x == SQLTableData[0].id) {
             console.log("the data is an integer");
         } else {
             console.log("It must be a string");
         }
-        // console.log(SQLTableData[0].quantity);
-        // console.log(SQLTableData[0].product_name);
-        // SQLTableDataName=SQLTableData[0].product_name;
-        // console.log('SQLTableDataName:', SQLTableDataName)
-        // console.log("Hopefully there is a response above!!");
-        // console.log('SQLTableData 1:', SQLTableData[1]);
         buyBuyBuy();
         // connection.end();
     });
-
 }
 function buyBuyBuy() {
     inquirer.prompt([
@@ -68,61 +59,51 @@ function buyBuyBuy() {
             type: "input",
             message: "What is the ID of the product you want to buy? "
         },
-
         {
             name: "quantity",
             type: "input",
             message: "How many would you like?"
         }
     ]).then(function (answer) {
-        //console.log(answer);
         userItemID = answer.itemID;
         userQuantity = answer.quantity;
-  
-
         for (i = 0; i < SQLTableData.length; i++) {
-            //var b = i + 1;
             if (userItemID == SQLTableData[i].id) {
                 userItemID = i;
                 console.log(SQLTableData[userItemID]);
-
             }
         }
-        // console.log('itemID after loop', userItemID);
         itemName = SQLTableData[userItemID].product_name;
         console.log('itemName:', itemName);
         newTableDataQuantity = SQLTableData[userItemID].quantity - userQuantity;
         console.log('newTableDataQuantity:', newTableDataQuantity);
         if (0>newTableDataQuantity){
             console.log("Sorry! Insufficient quantity of reserves! Your order needs to be reduced!"); 
-        }else updateProduct(newTableDataQuantity);
-
+        }else updateProduct();
     });
-
-
 };
-
-
 function updateProduct() {
-    console.log("Updating all Rocky Road quantities...\n");
+    console.log("Updating "+ itemName + " quantities...\n");
     var query = connection.query(
         "UPDATE products SET ? WHERE ?",
         [
             {
-                quantity: 100
+                quantity: newTableDataQuantity
             },
             {
-                flavor: "Rocky Road"
+                product_name: itemName
             }
         ],
         function (err, res) {
             if (err) throw err;
             console.log(res.affectedRows + " products updated!\n");
             // Call deleteProduct AFTER the UPDATE completes
-            deleteProduct();
+            //deleteProduct();
         }
     );
 }
+
+//userItemID+1
     // function checkCheckCheck() {
     //    // var query = "SELECT * FROM products top5000 WHERE ?";
     //    // connection.query(query, { artist: answer.artist }
